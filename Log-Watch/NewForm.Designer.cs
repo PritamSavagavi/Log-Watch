11877ef2a8882fc63a4de82fb453f824ddae3534
PritamSavagavi <pritamsavagavi@gmail.com>
2024-06-19 13:32:03 +0530

    close icon changed

--- Log-Watch/NewForm.Designer.cs
+++ Log-Watch/NewForm.Designer.cs
@@ -71,7 +71,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::Log_Watch.Properties.Resources.close;
+            this.pictureBox1.Image = global::Log_Watch.Properties.Resources.close1;
             this.pictureBox1.Location = new System.Drawing.Point(744, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(47, 34);
--- Log-Watch/Properties/Resources.Designer.cs
+++ Log-Watch/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Log_Watch.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap close1 {
+            get {
+                object obj = ResourceManager.GetObject("close1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap settings {
             get {
                 object obj = ResourceManager.GetObject("settings", resourceCulture);
