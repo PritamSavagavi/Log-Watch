0758b2545a1e8a0b17e37218649edb4ec1a70bf1
PritamSavagavi <pritamsavagavi@gmail.com>
2024-06-19 13:14:21 +0530

    Add project files.

---
+++ Log-Watch/NewForm.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Log_Watch
+{
+    partial class NewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.DGLogs = new System.Windows.Forms.DataGridView();
+            this.TimeStamp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Log = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblTargetPath = new System.Windows.Forms.Label();
+            this.txtTargetPath = new System.Windows.Forms.TextBox();
+            this.btnSetTargetFile = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lblTaskName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pnlBatchSeconds = new System.Windows.Forms.Panel();
+            this.btnSeconds = new System.Windows.Forms.Button();
+            this.txtBatchSeconds = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGLogs)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.pnlBatchSeconds.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.DGLogs);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(380, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(791, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Log_Watch.Properties.Resources.close;
+            this.pictureBox1.Location = new System.Drawing.Point(744, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(47, 34);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // DGLogs
+            // 
+            this.DGLogs.AllowUserToAddRows = false;
+            this.DGLogs.AllowUserToDeleteRows = false;
+            this.DGLogs.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.DGLogs.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.DGLogs.BackgroundColor = System.Drawing.Color.LightSteelBlue;
+            this.DGLogs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGLogs.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TimeStamp,
+            this.Log});
+            this.DGLogs.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DGLogs.Location = new System.Drawing.Point(3, 57);
+            this.DGLogs.Name = "DGLogs";
+            this.DGLogs.ReadOnly = true;
+            this.DGLogs.Size = new System.Drawing.Size(788, 390);
+            this.DGLogs.TabIndex = 1;
+            // 
+            // TimeStamp
+            // 
+            this.TimeStamp.HeaderText = "TimeStamp";
+            this.TimeStamp.Name = "TimeStamp";
+            this.TimeStamp.ReadOnly = true;
+            this.TimeStamp.Width = 208;
+            // 
+            // Log
+            // 
+            this.Log.HeaderText = "Log";
+            this.Log.Name = "Log";
+            this.Log.ReadOnly = true;
+            this.Log.Width = 537;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label1.Location = new System.Drawing.Point(343, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Changes in logs";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.lblTargetPath);
+            this.panel2.Controls.Add(this.txtTargetPath);
+            this.panel2.Location = new System.Drawing.Point(12, 239);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(317, 97);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblTargetPath
+            // 
+            this.lblTargetPath.AutoSize = true;
+            this.lblTargetPath.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTargetPath.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTargetPath.Location = new System.Drawing.Point(12, 64);
+            this.lblTargetPath.Name = "lblTargetPath";
+            this.lblTargetPath.Size = new System.Drawing.Size(180, 17);
+            this.lblTargetPath.TabIndex = 1;
+            this.lblTargetPath.Text = "Please enter target file path";
+            // 
+            // txtTargetPath
+            // 
+            this.txtTargetPath.Location = new System.Drawing.Point(15, 21);
+            this.txtTargetPath.Name = "txtTargetPath";
+            this.txtTargetPath.Size = new System.Drawing.Size(288, 20);
+            this.txtTargetPath.TabIndex = 0;
+            // 
+            // btnSetTargetFile
+            // 
+            this.btnSetTargetFile.BackColor = System.Drawing.Color.Lavender;
+            this.btnSetTargetFile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSetTargetFile.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSetTargetFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetTargetFile.Location = new System.Drawing.Point(91, 378);
+            this.btnSetTargetFile.Name = "btnSetTargetFile";
+            this.btnSetTargetFile.Size = new System.Drawing.Size(150, 38);
+            this.btnSetTargetFile.TabIndex = 2;
+            this.btnSetTargetFile.Text = "Start";
+            this.btnSetTargetFile.UseVisualStyleBackColor = false;
+            this.btnSetTargetFile.Click += new System.EventHandler(this.btnSetTargetFile_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnStop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(91, 378);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(150, 38);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblHeader.Location = new System.Drawing.Point(24, 24);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(137, 30);
+            this.lblHeader.TabIndex = 4;
+            this.lblHeader.Text = "Log Watcher";
+            // 
+            // lblTaskName
+            // 
+            this.lblTaskName.AutoSize = true;
+            this.lblTaskName.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaskName.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTaskName.Location = new System.Drawing.Point(29, 72);
+            this.lblTaskName.Name = "lblTaskName";
+            this.lblTaskName.Size = new System.Drawing.Size(81, 17);
+            this.lblTaskName.TabIndex = 5;
+            this.lblTaskName.Text = "Assignment";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(275, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Pritam Savagavi";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Log_Watch.Properties.Resources.settings;
+            this.pictureBox2.Location = new System.Drawing.Point(328, 9);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 28);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pnlBatchSeconds
+            // 
+            this.pnlBatchSeconds.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.pnlBatchSeconds.Controls.Add(this.btnSeconds);
+            this.pnlBatchSeconds.Controls.Add(this.label3);
+            this.pnlBatchSeconds.Controls.Add(this.txtBatchSeconds);
+            this.pnlBatchSeconds.Location = new System.Drawing.Point(328, 43);
+            this.pnlBatchSeconds.Name = "pnlBatchSeconds";
+            this.pnlBatchSeconds.Size = new System.Drawing.Size(265, 97);
+            this.pnlBatchSeconds.TabIndex = 8;
+            // 
+            // btnSeconds
+            // 
+            this.btnSeconds.BackColor = System.Drawing.Color.Lavender;
+            this.btnSeconds.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSeconds.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeconds.Location = new System.Drawing.Point(151, 54);
+            this.btnSeconds.Name = "btnSeconds";
+            this.btnSeconds.Size = new System.Drawing.Size(92, 27);
+            this.btnSeconds.TabIndex = 9;
+            this.btnSeconds.Text = "Save";
+            this.btnSeconds.UseVisualStyleBackColor = false;
+            this.btnSeconds.Click += new System.EventHandler(this.btnSeconds_Click);
+            // 
+            // txtBatchSeconds
+            // 
+            this.txtBatchSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBatchSeconds.Location = new System.Drawing.Point(21, 58);
+            this.txtBatchSeconds.Name = "txtBatchSeconds";
+            this.txtBatchSeconds.Size = new System.Drawing.Size(115, 22);
+            this.txtBatchSeconds.TabIndex = 0;
+            this.txtBatchSeconds.Text = "15";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(18, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Please enter batch seconds";
+            // 
+            // NewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1171, 450);
+            this.Controls.Add(this.pnlBatchSeconds);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTaskName);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.btnSetTargetFile);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnStop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NewForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Watch-Log";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGLogs)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.pnlBatchSeconds.ResumeLayout(false);
+            this.pnlBatchSeconds.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView DGLogs;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtTargetPath;
+        private System.Windows.Forms.Label lblTargetPath;
+        private System.Windows.Forms.Button btnSetTargetFile;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TimeStamp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Log;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lblTaskName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel pnlBatchSeconds;
+        private System.Windows.Forms.Button btnSeconds;
+        private System.Windows.Forms.TextBox txtBatchSeconds;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Log-Watch/NewForm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Log_Watch
+{
+    public partial class NewForm : Form
+    {
+        private FileSystemWatcher _fileWatcher;
+        private Timer _rotationTimer;
+        private string _currentLogFilePath;
+        private string _lastReadContent;
+        private int _batchSeconds;
+        public NewForm()
+        {
+            InitializeComponent();
+            pnlBatchSeconds.Visible = false;
+            _batchSeconds = 15;
+            InitializeRotationTimer();
+            txtBatchSeconds.KeyPress += TxtBatchSeconds_KeyPress;
+        }
+        private void TxtBatchSeconds_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow only control characters (e.g., backspace), digits, and optionally a single dot for decimals
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // Suppress the character
+            }
+        }
+        private void InitializeRotationTimer()
+        {
+            _rotationTimer = new Timer();
+            _rotationTimer.Interval = _batchSeconds;
+            _rotationTimer.Tick += OnRotationTimer_Tick;
+        }
+        
+        private void InitializeFileWatcher()
+        {
+            _fileWatcher = new FileSystemWatcher();
+            _fileWatcher.Path = Path.GetDirectoryName(_currentLogFilePath);
+            _fileWatcher.Filter = Path.GetFileName(_currentLogFilePath);
+
+            //this will update the change as soon as the file gets modified
+            //_fileWatcher.Changed += OnChanged;
+
+            _fileWatcher.EnableRaisingEvents = true;
+        }
+        private void OnRotationTimer_Tick(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_currentLogFilePath) && File.Exists(_currentLogFilePath))
+            {
+                ChackForChanges();
+            }
+        }
+
+        private void ChackForChanges()
+        {
+            try
+            {
+                string currentContent = File.ReadAllText(_currentLogFilePath);
+                if (_lastReadContent != currentContent)
+                {
+                    ReportChanges(_lastReadContent, currentContent);
+                    _lastReadContent = currentContent;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
+        }
+        private void ReportChanges(string oldContent, string newContent)
+        {
+            string changes = newContent;
+            if (!string.IsNullOrWhiteSpace(changes))
+            {
+                LogChanges(newContent);
+                //DGLogs.Rows.Add(DateTime.Now, changes);
+            }
+        }
+
+        private void LogChanges(string newContent)
+        {
+            var oldLines = _lastReadContent.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            var newLines = newContent.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            var addedLines = newLines.Except(oldLines).ToList();
+            var removedLines = oldLines.Except(newLines).ToList();
+
+            StringBuilder changeLog = new StringBuilder();
+            if (addedLines.Any())
+            {
+                changeLog.AppendLine("\n Added lines : ");
+                foreach (var line in addedLines)
+                {
+                    changeLog.AppendLine(line);
+                }
+            }
+            if (removedLines.Any())
+            {
+                changeLog.AppendLine("\n Removed lines : ");
+                foreach (var line in removedLines)
+                {
+                    changeLog.AppendLine(line);
+                }
+            }
+
+            if (changeLog.Length > 0)
+            {
+                DGLogs.Rows.Add(DateTime.Now, changeLog.ToString());
+            }
+
+            _lastReadContent += newContent; // Update the last read content with new content
+        }
+
+        private void LogError(Exception ex)
+        {
+            File.AppendAllText("E:\\error.txt", $"{DateTime.Now}: {ex.Message}{Environment.NewLine}");
+        }
+
+        private void btnSetTargetFile_Click(object sender, EventArgs e)
+        {
+            _currentLogFilePath = txtTargetPath.Text;
+            if (File.Exists(_currentLogFilePath))
+            {
+                InitializeFileWatcher();
+                _lastReadContent = File.ReadAllText(_currentLogFilePath);
+                btnSetTargetFile.Visible = false;
+                btnStop.Visible = true;
+                _rotationTimer?.Start();
+            }
+            else
+            {
+                MessageBox.Show("The Specific file does not exist");
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _rotationTimer?.Stop();
+            btnStop.Visible = false;
+            btnSetTargetFile.Visible = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pnlBatchSeconds.Visible = true;
+        }
+
+        private void btnSeconds_Click(object sender, EventArgs e)
+        {
+            pnlBatchSeconds.Visible = false;
+            _batchSeconds = Convert.ToInt32(txtBatchSeconds.Text);
+            _rotationTimer.Stop();
+            _rotationTimer.Start();
+        }
+    }
+}
